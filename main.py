3f4b1214a4c6a4ee09ce99dfceeb419bceaf8c29
steezeman <github@davecutting.com>
2024-04-26 18:06:31 -0500

    Update SWC read

--- main.py
+++ main.py
@@ -3,16 +3,95 @@ import socket
 from pynmeagps import NMEAReader
 import geopy.distance as distance
 import home
+from threading import Thread
 import time
 
+
+def EnableStarlink(on):
+    libioplus.setRelayCh(0, 7, on)
+
+
+def EnableCompressor(on):
+    libioplus.setRelayCh(0, 1, on)
+    libioplus.setRelayCh(0, 2, on)
+
+def EnableHighPowerCharge(iso, non):
+    # Disable the chargers
+    libioplus.setRelayCh(0, 3, iso)
+    libioplus.setRelayCh(0, 4, non)
+
+    if iso or non:
+        libioplus.setOdPwm(0, 1, 10000)
+    else:
+        libioplus.setOdPwm(0, 1, 0)
+
+def GetAccessoryState():
+    return libioplus.getOptoCh(0, 5)
+
+def GetSwcState():
+    testkey = ["",""]
+    confidentkey = ["",""]
+    oldkey = ["",""]
+
+    while(1):
+        swc = (libioplus.getAdcV(0, 1), libioplus.getAdcV(0, 2))
+
+        oldkey[0] = testkey[0]
+
+        if swc[0] < 0.03:
+            testkey[0] = "MODE"
+        elif swc[0] < 0.1:
+            testkey[0] = "HANG UP"
+        elif swc[0] < 0.3:
+            testkey[0] = "ANSWER"
+        elif swc[0] < 0.7:
+            testkey[0] = "SIRI"
+        else:
+            testkey[0] = ""
+            confidentkey[0] = ""
+
+        oldkey[1] = testkey[1]
+
+        if swc[1] < 0.03:
+            testkey[1] = "UP"
+        elif swc[1] < 0.1:
+            testkey[1] = "DOWN"
+        elif swc[1] < 0.3:
+            testkey[1] = "VOL+"
+        elif swc[1] < 0.7:
+            testkey[1] = "VOL-"
+        else:
+            testkey[1] = ""
+            confidentkey[1] = ""
+
+        if oldkey[0] == testkey[0] and confidentkey[0] == "" and testkey[0] != "":
+            confidentkey[0] = testkey[0]
+            print(confidentkey[0])
+
+        if oldkey[1] == testkey[1] and confidentkey[1] == "" and testkey[1] != "":
+            confidentkey[1] = testkey[1]
+            print(confidentkey[1])
+
+        if confidentkey[0] != "" or confidentkey[0] != "":
+            # Do some HID stuff
+            pass
+
+        time.sleep(0.01)
+
+
+
 if __name__ == '__main__':
+    thread = Thread(target=GetSwcState)
+    thread.start()
+
     stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     stream.connect(("192.168.50.1", 60660))
     nmr = NMEAReader(stream)
-    ignition = False
 
     # Set the engine idle high (10000) or low (0)
     libioplus.setOdPwm(0, 1, 0)
+
+    EnableCompressor(True)
 
     for (raw_data, parsed_data) in nmr:
         if parsed_data.msgID == "RMC":
@@ -20,33 +99,23 @@ if __name__ == '__main__':
             coords_2 = (parsed_data.lat, parsed_data.lon)
 
             distance_from_home = distance.distance(coords_1, coords_2).km
-            # channel 5 is accessory, labeled backwards on the site
-            accessory = libioplus.getOptoCh(0, 5)
-            # swc1 is aux, swc2 is arrow pad
-            swc1 = libioplus.getAdcV(0, 1)
-            swc2 = libioplus.getAdcV(0, 2)
+            accessory = GetAccessoryState()
 
-            print(f" {distance_from_home} km from home, accessory is {accessory}, volts swc1: {swc1} swc2: {swc2}")
-            if distance_from_home > 0.1 or accessory:
-                libioplus.setRelayCh(0, 7, 1)
-                print("Turning starlink on")
+            print(f" {distance_from_home} km from home, accessory is {accessory}")
+            if distance_from_home > 1.0 or accessory:
+                EnableStarlink(True)
             else:
-                libioplus.setRelayCh(0, 7, 0)
-                print("Turning starlink off")
+                EnableStarlink(False)
 
             if accessory:
-                # Enable the compressor
-                libioplus.setRelayCh(0, 3, 1)
-                libioplus.setRelayCh(0, 4, 1)
-
-                # Make the truck idle higher
-                # Todo: add behavior to make this only happen when charging at >50% of max rate
-                libioplus.setOdPwm(0, 1, 10000)
+                EnableHighPowerCharge(True, True)
             else:
-                # Disable the compressor
-                libioplus.setRelayCh(0, 3, 0)
-                libioplus.setRelayCh(0, 4, 0)
+                EnableHighPowerCharge(False, False)
 
-                # Make the truck idle higher
-                libioplus.setOdPwm(0, 1, 0)
 
+
+            # if alternator_current > 50:
+            #     EnableHighPowerCharge(True, True)
+
+
+    thread.join()
